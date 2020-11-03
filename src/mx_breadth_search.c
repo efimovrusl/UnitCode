d3ce47decf92ac4495cd37ba1f34db4d6e5c878b
pen_is_wise <efimovrusl@gmail.com>
2020-11-03 02:05:57 +0200

    finally working search

--- src/mx_breadth_search.c
+++ src/mx_breadth_search.c
@@ -1,75 +1,46 @@
 #include "pathfinder.h"
 
-void mx_multiply_and_join(t_list **paths, t_list *new_paths);
-t_list *mx_search(vertex ***vertexes, t_list *checked, vertex *required);
+void mx_search(vertex ***vertexes, t_list *checked, vertex *required, t_list **res_paths);
 
 // needs NULL-terminated matrix
 t_list *mx_breadth_search(vertex ***vertexes, vertex *initial, vertex *required) {
     t_list *checked = NULL;
     mx_push_front(&checked, initial);
-    printf("let's begin!\n");
-    t_list *paths = mx_search(vertexes, checked, required);
-    printf("WE FOUND SMTH!\n");
+    t_list *paths = NULL;
+    mx_search(vertexes, checked, required, &paths);
+    t_list *buf = paths;
+    while (buf) {
+        t_list *lol = (t_list *)(buf->data);
+        printf("path:\n");
+        while (lol) {
+            printf("%s ", ((vertex *)(lol->data))->name);
+            lol = lol->next;
+        }
+        printf("\n");
+        buf = buf->next;
+    }
     /**
      * @brief 
-     * process paths list
-     * return list of shortest paths
+     * @https://github.com/efimovrusl list of shortest paths
      */
     return NULL;
 }
 
-t_list *mx_search(vertex ***vertexes, t_list *checked, vertex *required) {
-    printf("nice ");
+void mx_search(vertex ***vertexes, t_list *checked, vertex *required, t_list **res_paths) {
     if ((vertex *)(checked->data) == required) {
-        mx_clear_list(&checked);
-        printf("WE FOUND PATH!\n");
-        return NULL;
+        mx_push_front(res_paths, checked);
+        return;
     }
-    printf("done!\n\t");
-
-    t_list *buffer = checked;
-    while (buffer) {
-        printf("%s, ", ((vertex *)(buffer->data))->name);
-        buffer = buffer->next;
-    }
-    printf("\n");
-
-    t_list *paths = NULL, *primal_paths = NULL;
+    t_list *paths = NULL;
     int line_id = mx_get_id(vertexes, (vertex *)(checked->data));
     vertex **line = vertexes[line_id];
-    for (int i = 1; line[i]; i++) {
+    for (int i = 1; (unsigned long)(void *)line[i] > (unsigned long)0xff; i++) {
         if (!mx_has_vertex_address(checked, line[i])) {
-            printf("\tname: %s\n", line[i]->name);
             t_list *new_checked = mx_list_cpy(checked);
             mx_push_front(&new_checked, line[i]);
-            t_list *res = mx_search(vertexes, new_checked, required);
-            mx_push_front(&res, line[i]);
-            printf("fuck lol! *res: %p\n", (void *)res);
-            mx_multiply_and_join(&paths, res);
-            if (!primal_paths && paths)
-                primal_paths = paths;
-            paths = paths->next;
-        }
-    }
-    printf("primal paths:\n");
-    mx_pop_front(&primal_paths);
-    return primal_paths;
-}
-
-void mx_multiply_and_join(t_list **paths, t_list *new_paths) {
-    printf("multiply & join args: %p %p\n", (void *)((*paths)), (void *)new_paths);
-    t_list *primal = NULL;
-    if (*paths)
-        primal = (t_list *)((*paths)->data);
-    
-
-    while (new_paths) {
-        t_list *temp = mx_list_cpy(primal);
-        if (temp)
-            mx_get_back(temp)->next = new_paths;
-        else
-            temp = new_paths;
-        mx_push_back(paths, temp);
-        new_paths = new_paths->next;
+            mx_search(vertexes, new_checked, required, res_paths);
+            if ((vertex *)(new_checked->data) != required)
+                mx_clear_list(&new_checked);
+        }   
     }
 }
